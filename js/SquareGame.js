e9f03fde02b81ddc6fc041cf49b26a0709fedc1a
mrgenco <mrgenco@gmail.com>
2015-06-08 23:54:22 +0300

    points added to canvas

--- js/SquareGame.js
+++ js/SquareGame.js
@@ -1,21 +1,81 @@
-// Global vars
+//Author : Mehmet Rasid Gencosmanoglu
+//ProjectName : SquareGame
 
-var buttonStart = document.getElementById("start-button");
+
+
+// Global variables
 canvas = null;
 ctx = null;
 
+var isStarted = false;
+
+var pointCountX = 5;
+var pointCountY = 5;
+var canvasWidth;
+var canvasHeight;
+
+var buttonStart = document.getElementById("start-button");
 buttonStart.onclick = startGame;
-
-
-
 // ----------------------------------------
 function startGame(){
 
-	canvas = document.getElementById("game-canvas");
-    ctx = canvas.getContext("2d");
-    
-    ctx.strokeRect(40, 40, 80, 40);
-    ctx.fillText("Hello World!", 50, 65);
 
+	if(isStarted)
+	{
+		alert("Game is already started!");
+	}
+	else{
+
+		canvas = document.getElementById("game-canvas");
+	    ctx = canvas.getContext("2d");
+
+	    canvasWidth = canvas.width;
+	    canvasHeight = canvas.height;
+
+	    //alert("Canvas width = "+canvasWidth+" \nCanvas height = "+canvasHeight);
+
+	    var countX = pointCountX;
+	    var countY = pointCountY;
+
+	    //Points are created on the board
+	    pointDistanceX = canvasWidth/pointCountX;
+	    pointDistanceY = canvasHeight/pointCountY;
+
+	    //pointX and pointY should start from 5 since 
+	    //the gameboard has a 5px border
+	    pointX = 5;	
+	    pointY = 5;
+
+	    for (var i = 0; i < countY; i++) {
+	    	for (var j = 0; j < countX; j++) {
+	    		
+	    		drawPointCircle(pointX, pointY, ctx);
+	    		pointX = pointX + pointDistanceX;
+
+	    	};
+	    	pointY = pointY + pointDistanceY;
+	    	pointX = 5;
+	    };
+
+		isStarted = true;
+	} 
+    
 }
+
+function drawPointRect(x, y, context){
+  context.fillRect(x,y,3,3);
+}
+
+function drawPointCircle(x, y, context){
+  context.beginPath();
+  context.arc(x, y, 3, 0, 2 * Math.PI, true);
+  context.fillStyle = 'gray';
+  context.fill();
+  context.lineWidth = 1;
+  context.strokeStyle = '#D5D0D0';
+  context.stroke();
+}
+
+
+
 
