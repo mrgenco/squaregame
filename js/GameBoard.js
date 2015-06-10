f6d30dc5138d310c09cd8e27b6ea5f0292403001
mrgenco <mrgenco@gmail.com>
2015-06-10 10:13:02 +0300

    added circles to gameboard

--- js/GameBoard.js
+++ js/GameBoard.js
@@ -4,8 +4,6 @@
 
 
 // Global variables
-canvas = null;
-ctx = null;
 var isStarted = false;
 
 var pointCountX = 5;
@@ -26,9 +24,8 @@ function startGame(){
 	{
 		alert("Game is already started!");
 	}
-	else{
-
-		
+	else
+	{
 
 	    canvasWidth = canvas.width;
 	    canvasHeight = canvas.height;
@@ -40,23 +37,24 @@ function startGame(){
 	    var countY = pointCountY;
 
 	    //Points are created on the board
-	    pointDistanceX = canvasWidth/pointCountX;
-	    pointDistanceY = canvasHeight/pointCountY;
+	    pointDistanceX = canvasWidth/countX;
+	    pointDistanceY = canvasHeight/countY;	
 
 	    //pointX and pointY should start from 5 since 
 	    //the gameboard has a 5px border
-	    pointX = 5;	
-	    pointY = 5;
+	    pointX = 3;	
+	    pointY = 3;
 
 	    for (var i = 0; i < countY; i++) {
 	    	for (var j = 0; j < countX; j++) {
 	    		
-	    		drawPointCircle(pointX, pointY, ctx);
-	    		pointX = pointX + pointDistanceX;
+    	      drawPointCircle(pointX, pointY, ctx);
+
+	    	  pointX = pointX + pointDistanceX;
 
 	    	};
 	    	pointY = pointY + pointDistanceY;
-	    	pointX = 5;
+	    	pointX = 3;
 	    };
 
 		isStarted = true;
@@ -64,42 +62,31 @@ function startGame(){
     
 }
 
-function drawPointRect(x, y, context){
-  context.fillRect(x,y,3,3);
+function drawPointCircle(x, y, context){
+	context.beginPath();
+	context.arc(x, y, 3, 0, Math.PI*2, false);
+	context.fillStyle = "red";
+	context.fill();
+	context.closePath();
+	
 }
 
-function drawPointCircle(x, y, context){
-  context.beginPath();
-  context.arc(x, y, 3, 0, 2 * Math.PI, true);
-  context.fillStyle = 'gray';
-  context.fill();
-  context.lineWidth = 1;
-  context.strokeStyle = '#D5D0D0';
-  context.stroke();
-}
 function getMousePos(canvas, evt) {
-        var rect = canvas.getBoundingClientRect();
-        return {
-          x: evt.clientX - rect.left,
-          y: evt.clientY - rect.top
-        };
-      }
+    var rect = canvas.getBoundingClientRect();
+    return {
+      x: evt.clientX - rect.left,
+      y: evt.clientY - rect.top
+    };
+}
 
 canvas.addEventListener('mousedown', function(evt) {
 	var mousePos = getMousePos(canvas, evt);
 	var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
 	console.log(message);
 }, false);
+	
 
 
-
-
-function writeMessage(canvas, message) {
-    var context = canvas.getContext('2d');
-    context.font = '10pt Calibri';
-    context.fillStyle = 'black';
-    context.fillText(message, 10, 25);
-}
 
 
 
